468fdec532d19236bc5e573d465a4220c0962b4c
Pietro907 <pietrop.puri@gmail.com>
2023-09-05 15:39:01 +0200

    add time now and time at tomorrow
    added const zeroTime and time Now and print on page nowDate

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,7 +18,31 @@ Esistono dei metodi per trasformare una data in millisecondi?
 //Che giorno è oggi?
 //Crea una constante e assegna la data odierna
 const nowDate = new Date();
-console.log(nowDate);
+console.log(nowDate + ' data odierna');
+const dateInMillisecond = new Date('Sept 5, 2023 15:00:00').getTime();
+console.log(dateInMillisecond + ' data Sept 5, 2023 15:00:00 espressa in mille secondi');
 
+//Associa e stampa a video la data di oggi nell'elemento now_date, della page HTML
 const nowDateElement = document.querySelector('.now_date');
-nowDateElement.innerHTML = nowDate;
+//nowDateElement.innerHTML = nowDate.toUTCString();
+
+//Quanto manca alle 9:30 di domani mattina
+// dalle 15 del pomeriggio alle 9:30 di domani
+const milliSecond = new Date(); //18.30 ore
+console.log(milliSecond + ' data calcolata');
+
+//Crea una constante e assegna il valore di partenza del tempo
+const zeroTime = 86400000;  // millisecondi (24h, un giorno) dal punto zero di js (January 01, 1970 00:00:00 UTC)
+console.log(zeroTime + ' Zero Time');
+const timeNow = 64800000;  // millisecondi alle 9:30 di domani
+console.log(timeNow + ' Time remaining');
+
+/* 
+JavaScript stores dates as number of milliseconds since January 01, 1970.
+
+Zero time is January 01, 1970 00:00:00 UTC.
+
+One day (24 hours) is 86 400 000 milliseconds.
+
+Now the time is: 1693917785994 milliseconds past January 01, 1970
+*/
